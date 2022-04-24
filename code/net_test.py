0d18a080e6b559cf45ad244370f4d0057c7597a4
Ashish Barnawal <ashbrnwl.2000@gmail.com>
2022-04-24 17:54:20 +0530

    update code

--- code/net_test.py
+++ code/net_test.py
@@ -1,4 +1,6 @@
+from cProfile import label
 from non_cooperative import Route, Player, Network
+import matplotlib.pyplot as plt
 
 r11 = Route([0, 4], [
     [0, 0],
@@ -24,8 +26,14 @@ r22 = Route([3, 0], [
 p0 = Player([r11, r12], 0)
 p1 = Player([r21, r22], 1)
 
-network = Network([20., 20.0], [p0, p1])
-print(network.waitTime(0))
+network = Network([20., 10.0], [p0, p1])
+# print(network.waitTime(0))
 # print(network.calcPureNashEquilibrium())
-network.correlatedEquilbrium()
+# print(network.correlatedEquilbrium())
 # print(network.calcDiscreteCosts())
+
+mwew = network.multiplicativeWeights(eta=0.5, nSteps=50)
+for i in range(len(mwew[0])):
+    plt.plot([x[i] for x in mwew], '.-', label=f'Player {i}')
+
+plt.show()
--- code/non_cooperative.py
+++ code/non_cooperative.py
@@ -1,10 +1,12 @@
 #!/bin/python3
 
 from typing import List
+from xml.etree.ElementInclude import include
 import numpy as np
 import scipy.optimize
 import itertools
 import pulp
+import math
 
 class Route:
     def __init__(self,gamma, R) -> None:
@@ -41,6 +43,7 @@ class Network:
         self.state = []
         self.numNodes = len(serviceRates)
         self.serviceRates = serviceRates
+        self.nPlayers = len(players)
         
         for player in players:
             self.__add_player(player)
@@ -48,7 +51,7 @@ class Network:
         self.C : List[List[List[float]]] = self.__calcC()
     
     def __add_player(self, player : Player) -> None:
-        if len(self.players) > 0:
+        if self.nPlayers > 0:
             assert(player.reqNodes == len(self.serviceRates))
         self.players.append(player)
         
@@ -60,9 +63,9 @@ class Network:
     def __calcC(self) -> List[List[List[float]]]:
         C = [[[0.0 for i in range(self.numNodes)]
                for r in range(self.players[k].nChoices)]
-                for k in range(len(self.players))]
+                for k in range(self.nPlayers)]
         
-        for k in range(len(self.players)):
+        for k in range(self.nPlayers):
             for r in range(self.players[k].nChoices):
                 for i in range(self.numNodes):
                     for j in range(self.numNodes):
@@ -81,7 +84,7 @@ class Network:
         # waits[i] = $\frac{1}{\mu_i - \sum_{k}\sum_{r} p^{(k,r)} C^{(k,r,i)}}$
         for i in range(self.numNodes):
             arrv = 0
-            for k in range(len(self.players)):
+            for k in range(self.nPlayers):
                 for r in range(self.players[k].nChoices):
                     arrv += strategies[k][r]*self.C[k][r][i]
             waits.append(1.0/(self.serviceRates[i] - arrv))
@@ -99,7 +102,7 @@ class Network:
     def checkStrategies(self, strategies):
         # 1. sum of probabilities is 1
         # 2. probabilities are >= 0
-        for j in range(len(self.players)):
+        for j in range(self.nPlayers):
             sm = 0
             for p in strategies[j]:
                 sm += p
@@ -109,7 +112,7 @@ class Network:
         # total arrival rate at node is less than service rate                        
         for i in range(self.numNodes):
             arrv = 0
-            for k in range(len(self.players)):
+            for k in range(self.nPlayers):
                 for r in range(self.players[k].nChoices):
                     arrv += strategies[k][r]*self.C[k][r][i]
             print(f"arrv[{i}] =  {arrv} (service = {self.serviceRates[i]}")
@@ -134,7 +137,7 @@ class Network:
                 A_ineq[i,j] = self.C[playerId][j][i]
             lb_ineq[i] = 0
             muhat = self.serviceRates[i]
-            for k in range(len(self.players)):
+            for k in range(self.nPlayers):
                 for r in range(self.players[k].nChoices):
                     muhat = muhat - self.state[k][r]*self.C[k][r][i]
             ub_ineq[i] = muhat
@@ -179,7 +182,7 @@ class Network:
             print(f"\nNash Iteration {iter}")
             anyNew = False
             print("current Strats : ", self.state)
-            for j in range(len(self.players)):
+            for j in range(self.nPlayers):
                 oldStrat = self.state[j]
                 self.state[j] = self.bestResponse(j)
                 print(f"new strat for player {j}", self.state[j])
@@ -198,7 +201,7 @@ class Network:
         cost = {}
         
         for strategy in strategySpace:
-            curCost = np.zeros(len(self.players))
+            curCost = np.zeros(self.nPlayers)
             for j, rj in enumerate(strategy):
                 self.state[j] = np.zeros((self.players[j].nChoices,))
                 self.state[j][rj] = 1.0
@@ -209,7 +212,7 @@ class Network:
                 else:
                     continue
             else:
-                for j in range(len(self.players)):
+                for j in range(self.nPlayers):
                     curCost[j] = self.waitTime(j)
             
             cost[strategy] = tuple(curCost.tolist())
@@ -223,7 +226,7 @@ class Network:
         M = 1000.0
         for s in cost:
             if cost[s][0] == np.infty:
-                cost[s] = tuple([M]*len(self.players))
+                cost[s] = tuple([M]*self.nPlayers)
         
         p = {}
         systemLoad = 0
@@ -264,11 +267,39 @@ class Network:
         
         print("Status:", pulp.LpStatus[prob.status])
         
-        for v in p:
-            print(v.name, "=", v.varValue)
+        res = {}
+        for s in p:
+            v = p[s]
+            res[s] = v.varValue
+
+        return res
     
     def discreteCooperativeOptimal(self):
         return min(map(sum, self.calcDiscreteCosts().values()))
     
-    # def continuousCooperativeOptimal(self):
+    def multiplicativeWeights(self, eta=0.2, nSteps=5):
+        costs = self.calcDiscreteCosts(includeNonFeasible=True)
+        states = list(costs.keys())
         
+        p = [[[1.0/pl.nChoices]*pl.nChoices for pl in self.players] for _ in range(nSteps+1)]
+        loss = [[[0]*pl.nChoices for pl in self.players] for _ in range(nSteps+1)]
+        EW = [[0]*self.nPlayers for _ in range(nSteps)]
+        
+        for t in range(nSteps):
+            for j in range(self.nPlayers):
+                for r in range(self.players[j].nChoices):
+                    loss[t][j][r] += sum(
+                        costs[s][j]*math.prod(p[t][k][s[k]] for k in range(self.nPlayers) if k != j) 
+                        for s in states if s[j] == r
+                    )
+                    p[t+1][j][r] = p[t][j][r]*math.pow(1-eta, loss[t][j][r])
+                
+                # normalize p[t+1][j]
+                sm = sum(p[t+1][j])
+                for r in range(self.players[j].nChoices):
+                    p[t+1][j][r] /= sm
+
+                
+                EW[t][j] = sum(loss[t][j][r]*p[t][j][r] for r in range(self.players[j].nChoices))
+        
+        return EW
